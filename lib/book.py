ccc36d5d8e351d2acc611bb39f2b0646dba2aa2f
winnieotieno770 <winnieotieno770@gmail.com>
2026-01-03 21:44:23 +0300

    Add Bok and hoe class with validation

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
+    def __init__(self,title,page_count):
+        self.title = title
+        self.page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
         
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self,title):
+        self._title = title
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, page_count):
+        if isinstance (page_count,int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+abi = Book("The crush", 30)
+p= abi.page_count
+print(p)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self,brand,size):
+        self.brand = brand
+        self.size = size
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+    
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self,size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, brand):
+         self._brand = brand
+
+stan_smith = Shoe("Adidas", 9)
+stan_smith.brand = "Nike"
+print(stan_smith.brand)
+
